9043524a02025950a3c694fd8834213af0f5ea40
WildanFahri <Wildan.fahri03@gmail.com>
2021-04-20 15:26:16 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
-Route::resource('articles', [ArticleController::class]);
+Route::resource('articles', ArticleController::class);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
