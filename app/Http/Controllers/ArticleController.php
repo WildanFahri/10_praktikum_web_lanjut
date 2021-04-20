b8ae5d7e9f33b54664fbc2114516fdcd9ab1e3f5
WildanFahri <Wildan.fahri03@gmail.com>
2021-04-20 15:31:50 +0700

    update function create

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -24,7 +24,7 @@ class ArticleController extends Controller
      */
     public function create(Request $request)
     {
-        return view('create');
+        return view('articles.create');
     }
 
     /**
